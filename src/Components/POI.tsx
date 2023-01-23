6022a58a653010191c0c9f563108e9375fd99f99
Eneman <hendrix.bcz@gmail.com>
2023-01-23 03:02:34 +0100

    - removed axios - added random planet names generator

--- src/Components/POI.tsx
+++ src/Components/POI.tsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { POIProps } from '../types';
+import planetNames from '../assets/planetNames.json';
+import cheerio from 'cheerio';
 
-/* export class POI extends React.Component<POIProps> {
-    render() {
-        return (
-            
+function getPlanetName(): string {
+    let nameSize: number = Math.floor(Math.random() * 11);
+    let nameParts: string[] = [];
+    for (let i = 0; i < 8; i++) {
+        nameParts.push(planetNames.name[i][Math.floor(Math.random() * planetNames.name[i].length)]);
+    }
+    while (nameParts[0] === nameParts[2]) {
+        nameParts.splice(
+            2,
+            1,
+            planetNames.name[2][Math.floor(Math.random() * planetNames.name[2].length)],
         );
     }
-} */
+    while (nameParts[0] === nameParts[3]) {
+        nameParts.splice(
+            2,
+            1,
+            planetNames.name[3][Math.floor(Math.random() * planetNames.name[3].length)],
+        );
+    }
+
+    if (nameSize < 2) {
+        return nameParts[0] + nameParts[1] + nameParts[2] + nameParts[4] + nameParts[5];
+    } else if (nameSize < 4) {
+        return nameParts[0] + nameParts[1] + nameParts[2] + nameParts[6];
+    } else if (nameSize < 6) {
+        return nameParts[0] + nameParts[4] + nameParts[5];
+    } else if (nameSize < 8) {
+        return nameParts[0] + nameParts[1] + nameParts[3] + nameParts[1] + nameParts[5];
+    } else {
+        return (
+            nameParts[3] +
+            nameParts[6] +
+            ' ' +
+            planetNames.name[7][Math.floor(Math.random() * planetNames.name[7].length)] +
+            planetNames.name[7][Math.floor(Math.random() * planetNames.name[7].length)] +
+            planetNames.name[7][Math.floor(Math.random() * planetNames.name[7].length)] +
+            planetNames.name[7][Math.floor(Math.random() * planetNames.name[7].length)]
+        );
+    }
+}
 
 export function POI(props: POIProps) {
     return (
         <div className="poi flex-none flex-shrink-0 basis-64 rounded-xl border-4 border-black bg-slate-600 p-3">
-            <h1 className="planetName mb-5 text-center text-4xl font-bold">Planet Name</h1>
+            <h1 className="planetName mb-5 text-center text-4xl font-bold capitalize">
+                {getPlanetName()}
+            </h1>
             <ul className="poi">
                 <li className="type">Type: {props.type}</li>
                 <li className="atmo">Atmosphere: {props.atmo}</li>
--- src/Components/POIGenerator.tsx
+++ src/Components/POIGenerator.tsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import cheerio from 'cheerio';
 import React from 'react';
 import worldData from '../assets/wold-data.json';
@@ -10,62 +9,6 @@ interface Choice {
     name: string;
     weight: number;
 }
-
-/* export class Generator extends React.Component {
-    weightedChoice(choices: Choice[]) {
-        let rndNum = Math.random() * choices[choices.length - 1].weight;
-        for (let i = 0; i < choices.length; i++) {
-            if (rndNum < choices[i].weight) {
-                return choices[i].name as string;
-            }
-        }
-    }
-
-    async getFNG() {
-        const resp = await axios.get('https://www.fantasynamegenerators.com/planet-names.php');
-        let name: string;
-        let $ = cheerio.load(resp.data);
-        name = $('div.result').text();
-        return (await name) as String;
-    }
-
-    render() {
-        let pois: POIProps[] = [];
-        for (let i = 0; i < 9; i++) {
-            let ele: POIProps = {
-                type: this.weightedChoice(worldData.type),
-                atmo: this.weightedChoice(worldData.atmo),
-                gravity: this.weightedChoice(worldData.gravity),
-                biome: this.weightedChoice(worldData.biome),
-                accord: this.weightedChoice(worldData.accord),
-                align: this.weightedChoice(worldData.alignement),
-                magic: this.weightedChoice(worldData.magic),
-                religion: this.weightedChoice(worldData.religion),
-                techno: this.weightedChoice(worldData.technology),
-            };
-            pois.push(ele);
-        }
-
-        return (
-            <div className="flex flex-1 grow basis-full flex-row flex-wrap justify-evenly gap-4 text-white">
-                {pois.map((e, i) => (
-                    <POI
-                        key={i}
-                        type={e.type}
-                        atmo={e.atmo}
-                        gravity={e.gravity}
-                        biome={e.biome}
-                        accord={e.accord}
-                        align={e.align}
-                        magic={e.magic}
-                        religion={e.religion}
-                        techno={e.techno}
-                    />
-                ))}
-            </div>
-        );
-    }
-} */
 
 function weightedChoice(choices: Choice[]) {
     let rndNum = Math.random() * choices[choices.length - 1].weight;
